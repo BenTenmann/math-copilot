92299c009d72bde0854a483b37427b684dd27edb
BenTenmann <benji.tenmann@me.com>
2023-08-06 14:21:46 +0100

    refactor(linter.py): - Rename `evaluate_latex_expression` to `expression_is_correct` for better semantics. - Extract `is_boolean` function to check if an expression is a boolean. - Extract `expression_to_bool` function to convert a boolean expression to a bool value. - Add type hints to `BOOL_MAP` dictionary. - Refactor the logic in `expression_is_correct` to handle different cases:   - Check if the expression was not parsed and return False.   - Check if the expression is a boolean and return the corresponding bool value.   - Substitute symbols in the expression and check if the substituted expression is a boolean.   - Simplify the substituted expression and check if the simplified expression is a boolean.   - If none of the above cases match, return True (the expression is correct). - Add a TODO comment to investigate better solvers.

--- backend/math_copilot/linter.py
+++ backend/math_copilot/linter.py
@@ -1,12 +1,43 @@
 import sympy
 import sympy.parsing.latex
+from sympy.logic import boolalg as ba
+
+BOOL_MAP: dict[
+    ba.BooleanTrue | ba.BooleanFalse,
+    bool
+] = {
+    ba.BooleanFalse: False,
+    ba.BooleanTrue: True,
+}
 
 
-def evaluate_latex_expression(latex_string: str) -> bool:
-    expression: sympy.Basic | bool = sympy.parsing.latex.parse_latex(latex_string)
-    if isinstance(expression, bool):
+def is_boolean(expression: sympy.Basic) -> bool:
+    return isinstance(expression, (ba.BooleanTrue, ba.BooleanFalse))
+
+
+def expression_to_bool(expression: ba.BooleanFalse | ba.BooleanTrue) -> bool:
+    return BOOL_MAP[type(expression)]
+
+
+def expression_is_correct(latex_string: str, symbols: dict[str, sympy.Symbol]) -> bool:
+    expression: sympy.Basic | ba.BooleanAtom | None = sympy.parsing.latex.parse_latex(latex_string)
+    if expression is None:
+        # the expression was not parsed
+        return False
+    if is_boolean(expression):
         # these are trivial cases
-        return expression
+        return expression_to_bool(expression)
 
-    if isinstance(expression, sympy.Equality):
-        pass
+    substituted_expression = expression.subs(symbols)
+    if is_boolean(substituted_expression):
+        # with constraints imposed by the symbol definitions, the expression could be evaluated
+        return expression_to_bool(substituted_expression)
+
+    simplified_expression = sympy.simplify(substituted_expression)
+    if is_boolean(simplified_expression):
+        return expression_to_bool(simplified_expression)
+
+    # otherwise, the expression is correct (*)
+    # i.e. it is not possible to trivially show the expression is incorrect
+    # TODO: see if there are better solvers out there
+    return True
