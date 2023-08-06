f904412e8a34f9e570807775932cb496d2dad8ca
BenTenmann <benji.tenmann@me.com>
2023-08-06 16:11:15 +0100

    refactor(app.py): remove unused imports and commented out code feat(app.py): add Response model for API response feat(app.py): add POST endpoint '/latex' for converting image to latex expression refactor(app.py): remove unused code and dependencies in image_to_latex function refactor(app.py): simplify response handling and return Response model

--- backend/math_copilot/app.py
+++ backend/math_copilot/app.py
@@ -3,15 +3,13 @@ import json
 import logging
 import os
 from typing import Final
-from math_copilot.LLM import LLM
 
-import gradio as gr
-# import fastapi
-import numpy as np
+import pydantic
+
+import fastapi
 import requests
 from dotenv import load_dotenv, find_dotenv
-# from fastapi.middleware.cors import CORSMiddleware
-from PIL import Image
+from fastapi.middleware.cors import CORSMiddleware
 
 from math_copilot import linter, utils
 
@@ -21,29 +19,25 @@ LOGGER: Final[logging.Logger] = utils.get_logger(__name__)
 
 MATHPIX_APP_ID: Final[str | None] = os.environ.get("MATHPIX_APP_ID")
 MATHPIX_APP_KEY: Final[str | None] = os.environ.get("MATHPIX_APP_KEY")
-#
-# app = fastapi.FastAPI()
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=["*"],
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
-#
-#
-# @app.post("/latex")
-# def image_to_latex():
-#     pass
+
+app = fastapi.FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
-def fn(img: np.ndarray) -> tuple[str, list[tuple[str, str]], str]:
+class Response(pydantic.BaseModel):
+    latex: str
+    is_correct: bool
+
+
+@app.post("/latex")
+def image_to_latex(data) -> Response:
     assert MATHPIX_APP_ID is not None
     assert MATHPIX_APP_KEY is not None
-
-    image = Image.fromarray(img)
-    filehandle = io.BytesIO()
-    image.save(filehandle, "jpeg")
-    filehandle.seek(0)
     headers = {
         "app_id": MATHPIX_APP_ID,
         "app_key": MATHPIX_APP_KEY,
@@ -58,43 +52,12 @@ def fn(img: np.ndarray) -> tuple[str, list[tuple[str, str]], str]:
                 }
             )
         },
-        files={"file": filehandle},
+        files={"file": data},
         headers=headers,
     )
     response.raise_for_status()
     latex_expression = response.json()["latex_styled"]
-    isValid =linter.latex_expression_is_correct(latex_expression, {})
-    if not isValid:
-        llm = LLM()
-        llm_response = llm.explain_error(latex_expression)
-        print(llm_response)
-    else:
-        llm_response = ""
-    is_correct = str(isValid)
-    if "$" not in latex_expression:
-        latex_expression = f"$$\n{latex_expression}\n$$"
-    return latex_expression, [(is_correct, is_correct)], llm_response
-
-
-def main():
-    gr.Interface(
-        fn,
-        inputs=gr.components.Image(
-            shape=(224, 224),
-            image_mode="L",
-            invert_colors=False,
-            source="canvas",
-            tool="brush",
-            brush_radius=10,
-        ),
-        outputs=[
-            gr.Markdown(),
-            gr.HighlightedText().style(color_map={"True": "green", "False": "red"}),
-            gr.Markdown()
-        ],
-        title="Math Copilot",
-    ).launch()
-
-
-if __name__ == "__main__":
-    main()
+    return Response(
+        latex=latex_expression,
+        is_correct=linter.latex_expression_is_correct(latex_expression, {})
+    )
