81a5f8d0bf68aac298cae98cfb5fd4f210d83036
sradc <sidneyradcliffe@sky.com>
2023-08-06 15:15:42 +0100

    can draw, move and undo

--- frontend3/script.js
+++ frontend3/script.js
@@ -1,70 +1,61 @@
 window.addEventListener('DOMContentLoaded', (event) => {
-    // Initialize the canvas
-    var canvas = new fabric.Canvas('canvas');
-  
-    // Set the brush properties
-    canvas.isDrawingMode = true;
-    canvas.freeDrawingBrush.width = 5;
-    canvas.freeDrawingBrush.color = '#000000';
-  
-    // Adjust the canvas size to fit the window
-    function resizeCanvas() {
-      canvas.setWidth(window.innerWidth);
-      canvas.setHeight(window.innerHeight);
+  var canvas = new fabric.Canvas('canvas');
+  canvas.isDrawingMode = true;
+  canvas.freeDrawingBrush.width = 5;
+  canvas.freeDrawingBrush.color = '#000000';
+
+  function resizeCanvas() {
+    canvas.setWidth(window.innerWidth);
+    canvas.setHeight(window.innerHeight);
+    canvas.renderAll();
+  }
+  resizeCanvas();
+  window.addEventListener('resize', resizeCanvas);
+
+  function undo() {
+    var objects = canvas.getObjects();
+    if (objects.length > 0) {
+      canvas.remove(objects[objects.length - 1]);
       canvas.renderAll();
     }
-    resizeCanvas();
-    window.addEventListener('resize', resizeCanvas);
-  
-    // Save the canvas as an image
-    function saveCanvas() {
-      canvas.isDrawingMode = false;
-      var dataURL = canvas.toDataURL({ format: 'png' });
-      var link = document.createElement('a');
-      link.href = dataURL;
-      link.download = 'canvas_image.png';
-      link.click();
-      canvas.isDrawingMode = true;
+  }
+
+  var undoButton = document.getElementById('undo-button');
+  undoButton.addEventListener('click', undo);
+
+  var drawButton = document.getElementById('draw-button');
+  drawButton.addEventListener('click', function() {
+    canvas.isDrawingMode = true;
+  });
+
+  var moveButton = document.getElementById('move-button');
+  moveButton.addEventListener('click', function() {
+    canvas.isDrawingMode = false;
+  });
+
+  canvas.on('mouse:down', function (options) {
+    if (!canvas.isDrawingMode) {
+      this.isDragging = true;
+      this.selection = false;
+      this.lastPosX = options.e.clientX;
+      this.lastPosY = options.e.clientY;
     }
-  
-    // Undo the last drawing action
-    function undo() {
-      var objects = canvas.getObjects();
-      if (objects.length > 0) {
-        canvas.remove(objects[objects.length - 1]);
-        canvas.renderAll();
-      }
+  });
+
+  canvas.on('mouse:move', function (options) {
+    if (this.isDragging) {
+      var e = options.e;
+      var vpt = this.viewportTransform;
+      vpt[4] += e.clientX - this.lastPosX;
+      vpt[5] += e.clientY - this.lastPosY;
+      this.requestRenderAll();
+      this.lastPosX = e.clientX;
+      this.lastPosY = e.clientY;
     }
-  
-    // Event listener for the Undo button
-    var undoButton = document.getElementById('undo-button');
-    undoButton.addEventListener('click', undo);
-  
-    // Add the ability to move the view of the canvas
-    canvas.on('mouse:down', function (options) {
-      var evt = options.e;
-      if (evt.altKey === true) {
-        this.isDragging = true;
-        this.selection = false;
-        this.lastPosX = evt.clientX;
-        this.lastPosY = evt.clientY;
-      }
-    });
-  
-    canvas.on('mouse:move', function (options) {
-      if (this.isDragging) {
-        var e = options.e;
-        var vpt = this.viewportTransform;
-        vpt[4] += e.clientX - this.lastPosX;
-        vpt[5] += e.clientY - this.lastPosY;
-        this.requestRenderAll();
-        this.lastPosX = e.clientX;
-        this.lastPosY = e.clientY;
-      }
-    });
-  
-    canvas.on('mouse:up', function (options) {
-      this.isDragging = false;
-      this.selection = true;
-    });
+  });
+
+  canvas.on('mouse:up', function (options) {
+    this.isDragging = false;
+    this.selection = true;
+  });
 });
