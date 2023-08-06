9704cd155388c9dcb9f6518fc65cc912b6b14b68
sradc <sidneyradcliffe@sky.com>
2023-08-06 14:29:17 +0100

    removed unneeded nesting

--- frontend3/script.js
+++ frontend3/script.js
@@ -1,29 +1,27 @@
-window.addEventListener('DOMContentLoaded', (event) => {
-    // Initialize the canvas
-    var canvas = new fabric.Canvas('canvas');
+// Initialize the canvas
+var canvas = new fabric.Canvas('canvas');
 
-    // Set the brush properties
+// Set the brush properties
+canvas.isDrawingMode = true;
+canvas.freeDrawingBrush.width = 5;
+canvas.freeDrawingBrush.color = '#000000';
+
+// Adjust the canvas size to fit the window
+function resizeCanvas() {
+    canvas.setWidth(window.innerWidth);
+    canvas.setHeight(window.innerHeight);
+    canvas.renderAll();
+}
+resizeCanvas();
+window.addEventListener('resize', resizeCanvas);
+
+// Save the canvas as an image
+function saveCanvas() {
+    canvas.isDrawingMode = false;
+    var dataURL = canvas.toDataURL({ format: 'png' });
+    var link = document.createElement('a');
+    link.href = dataURL;
+    link.download = 'canvas_image.png';
+    link.click();
     canvas.isDrawingMode = true;
-    canvas.freeDrawingBrush.width = 5;
-    canvas.freeDrawingBrush.color = '#000000';
-
-    // Adjust the canvas size to fit the window
-    function resizeCanvas() {
-        canvas.setWidth(window.innerWidth);
-        canvas.setHeight(window.innerHeight);
-        canvas.renderAll();
-    }
-    resizeCanvas();
-    window.addEventListener('resize', resizeCanvas);
-
-    // Save the canvas as an image
-    function saveCanvas() {
-        canvas.isDrawingMode = false;
-        var dataURL = canvas.toDataURL({ format: 'png' });
-        var link = document.createElement('a');
-        link.href = dataURL;
-        link.download = 'canvas_image.png';
-        link.click();
-        canvas.isDrawingMode = true;
-    }
-});
+}
